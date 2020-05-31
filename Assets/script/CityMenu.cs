11db853e027fd5d1803c42937c4ad18f882990ad
fabianbremaud <fabianbremaud@gmail.com>
2020-05-31 15:40:36 +0200

    StartUI

---
+++ Assets/script/CityMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityMenu : MonoBehaviour
+{
+    Animator Anim;
+
+    private List<Unit> Units = new List<Unit>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Construct()
+    {
+        Anim.SetBool("IN", false);
+        Anim.SetBool("OUT", true);
+    }
+
+    public void CreateWarrior()
+    {
+        Anim.SetBool("OUT", false);
+        Anim.SetBool("IN", true);
+    }
+
+
+}
--- Assets/script/MainMapControllerScript.cs
+++ Assets/script/MainMapControllerScript.cs
@@ -6,10 +6,20 @@ public class MainMapControllerScript : MonoBehaviour
 {
     Camera _camera = null;
     Waypoint selectedWaypoint = null;
+    public GameObject cityPref;
     private List<City> _cities = new List<City>();
+
+
+    ///TESTSUI
+    private bool StartingCity;
+    public GameObject FileUI;
+    private Animator Anim;
+
     void Awake()
     {
         _camera = GetComponent<Camera>();
+
+        Anim = FileUI.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +31,20 @@ public class MainMapControllerScript : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                selectedWaypoint = hit.transform.gameObject.GetComponent<Waypoint>();
-                _cities.Add(new City(selectedWaypoint));         
-                Debug.Log("aaaaaaaaaa");
+                if (!StartingCity)
+                {
+                    selectedWaypoint = hit.transform.gameObject.GetComponent<Waypoint>();
+                    _cities.Add(new City(selectedWaypoint));
+                    Instantiate(cityPref, new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z), Quaternion.identity);
+                    StartingCity = true;
+                    Debug.Log("aaaaaaaaaa");
+                }
+                else if (hit.transform.gameObject.tag == "City")
+                {
+                    Debug.Log("Create UI!!!!");
+                    FileUI.SetActive(true);
+                    //Anim.SetBool("OUT", true);
+                }
             }
         }
     }
