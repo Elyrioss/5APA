707c4a3b0bb0ec8770279f8d0ef6f96e8c5114b2
Julien <judrsk@gmail.com>
2020-06-25 14:28:06 +0200

    new branch + temp title screen + update gitignore

---
+++ Assets/script/UI/MainMenuManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private bool loadScene = false;
+
+    [SerializeField]
+    private GameObject mainMenu;
+
+    [SerializeField]
+    private GameObject loadingMenu;
+    [SerializeField]
+    private Slider loadingSlider;
+
+    [SerializeField]
+    private int scene;
+    [SerializeField]
+    private TextMeshProUGUI loadingText;
+
+    private bool playClicked = false;
+
+    void Update()
+    {
+        if (playClicked && !loadScene)
+        {
+            mainMenu.SetActive(false);
+
+            loadingMenu.SetActive(true);
+
+            loadScene = true;
+            
+            loadingText.text = "LOADING...";
+            
+            StartCoroutine(LoadNewScene());
+        }
+        
+        if (loadScene == true)
+        {
+            loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
+        }
+
+    }
+    
+    IEnumerator LoadNewScene()
+    {
+        yield return new WaitForSeconds(3);
+        
+        AsyncOperation async = SceneManager.LoadSceneAsync(scene);
+        
+        while (!async.isDone)
+        {
+            float progress = Mathf.Clamp01(async.progress / 0.9f);
+
+            loadingSlider.value = progress;
+
+            yield return null;
+        }
+    }
+
+    public void PlayButtonClicked()
+    {
+        playClicked = true;
+    }
+
+    public void QuitButtonClicked()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
